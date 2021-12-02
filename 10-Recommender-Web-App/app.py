34353f22ab0bf9607a9bb8251e303cf1257508da
BGSoares <brunno.g.soares@gmail.com>
2021-12-02 18:06:53 +0100

    changes to week 10 project materials, including css file

--- 10-Recommender-Web-App/app.py
+++ 10-Recommender-Web-App/app.py
@@ -1,19 +1,28 @@
 
-from flask import Flask, render_template, request
-from recommender import recommend
+from flask import Flask, render_template, request, jsonify
+from recommender import recommend, MOVIES
+import random
 
 app = Flask("Brunnos Movie Recommender")
 
 # connects the URL "/" to the function hello()
-@app.route('/') # < - - a URL for a web page
+@app.route("/") # < - - a URL for a web page
 def hello_world():
-    return render_template('hello.html')
+    return render_template("hello.html")
 
-@app.route('/reco/')
+
+@app.route("/reco/")
 def recommendation():
-    genre = request.args['genre']
+    genre = request.args["genre"]
     movie = recommend(genre)
     return render_template("result.html", movie=movie)
+
+
+@app.route("/api/all/")
+def api_all_movies():
+    return jsonify(MOVIES)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 10-Recommender-Web-App/recommender.py
+++ 10-Recommender-Web-App/recommender.py
@@ -1,31 +1,13 @@
 
 import random
+import json
 
-MOVIES = {
-    'action':[
-        'Die Hard',
-        'Bruised',
-        'Red Notice',
-        'John Wick'
-    ],
-    'romance':[
-        'Titanic',
-        'Love Hard'
-    ],
-    'comedy':[
-        'Bad Santa',
-        'Kevin Alone at Home',
-        'Christmas Princess IV'
-    ],
-    'drama':[
-        'Into the Wild',
-        'The Other Son'
-    ]
-}
+f = open("data.json")
+MOVIES = json.load(f)
 
 def recommend(genre):
-    movie = MOVIES.get(genre, MOVIES['comedy'])
+    movie = MOVIES.get(genre, MOVIES["comedy"])
     return random.choice(movie)
 
-if __name__ == '__main__':
-    print(recommend('comedy'))
+if __name__ == "__main__":
+    print(recommend("comedy"))
